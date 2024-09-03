c113b553c4bf549646a10431e2e1a431fe5dfbc9
aidyninho <ryzyk.aidyn@gmail.com>
2024-09-04 03:14:59 +0500

    Add greeting rest controller

---
+++ src/main/java/com/andersen/hello/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package com.andersen.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping
+    public String greeting() {
+        return "hello";
+    }
+}
